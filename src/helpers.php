55fad67083dddfad5d0ac14a795489312ae99570
DESKTOP-G6TEB8E\wen <1562194488@qq.com>
2021-07-04 16:51:09 +0800

    'helpers.php'

--- src/helpers.php
+++ src/helpers.php
@@ -6,12 +6,15 @@
  * Time: 15:48
  */
 
+use WenRuns\Service\Button;
+
+
 if (function_exists('buttons')) {
     function buttons(array $buttons, \Closure $clusre, $toString = false)
     {
         $html = $toString ? '' : [];
         foreach ($buttons as $key => $buttonText) {
-            $button = new \WenRuns\Service\Button($buttonText);
+            $button = new Button($buttonText);
             if (is_callable($clusre)) {
                 call_user_func($clusre, $button);
             }
